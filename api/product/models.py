2a18d935b6dacd64363e95dc959634814d66f879
Victor M. <virtus.mv@gmail.com>
2023-11-07 19:18:29 +0700

    add product model

--- api/product/models.py
+++ api/product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
