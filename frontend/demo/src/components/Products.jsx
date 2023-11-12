f1196099c30a3c4ef20349c45c2c5dc8195f41a2
Victor M. <virtus.mv@gmail.com>
2023-11-12 14:15:45 +0700

    change fetch options to window.location.host

--- frontend/demo/src/components/Products.jsx
+++ frontend/demo/src/components/Products.jsx
@@ -4,7 +4,7 @@ function Products() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/products")
+    fetch("http://" + window.location.host + ":8000/api/products")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
